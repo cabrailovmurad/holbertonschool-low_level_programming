f1db0b010e0f9f9ca6d6d09e7e0ecf9963af5575
cabrailovmurad <dmurad908@gmail.com>
2025-06-12 17:49:29 +0000

    prints the alphabet in lowercase

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,3 +1,7 @@
+/*
+ * File: 3-print_alphabets.c
+ */
+
 #include <stdio.h>
 
 /**
@@ -7,15 +11,15 @@
  */
 int main(void)
 {
-    char letter;
+	char letter;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-    for (letter = 'A'; letter <= 'Z'; letter++)
-        putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
