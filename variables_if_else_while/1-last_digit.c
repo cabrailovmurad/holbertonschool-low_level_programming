e7ce32e32005f21b6b6b9eb2427a0010822e7020
cabrailovmurad <dmurad908@gmail.com>
2025-06-12 16:33:20 +0000

    ergge

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,21 +13,22 @@ int main(void)
     int n;
     int last_digit;
 
-    srand(time(0));        // Initsializiruem generator sluchaynykh chisel
-    n = rand() - RAND_MAX / 2;  // Generiruyem sluchaynoye chislo ot -RAND_MAX/2 do +RAND_MAX/2
-    last_digit = n % 10;   // Poluchayem poslednyuyu cifru chisla
+    srand(time(0));        /* Initsializiruem generator sluchaynykh chisel */
+    n = rand() - RAND_MAX / 2;  /* Generiruyem sluchaynoye chislo ot -RAND_MAX/2 do +RAND_MAX/2 */
+    last_digit = n % 10;   /* Poluchayem poslednyuyu cifru chisla */
 
-    // Ispol'zuyem switch dlya proverki posledney cifry
-    switch (last_digit) {
-        case 0:
-            printf("Last digit of %d is %d and is 0\n", n, last_digit);
-            break;
-        case 1 ... 5:  // Otnositel'no 1 do 5
-            printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
-            break;
-        default:  // Vse chisla, bol'she 5 ili otnosit'sya k otnositelnym
-            printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-            break;
+    /* Ispol'zuyem if-else dlya proverki posledney cifry */
+    if (last_digit == 0)
+    {
+        printf("Last digit of %d is %d and is 0\n", n, last_digit);
+    }
+    else if (last_digit > 0 && last_digit < 6)
+    {
+        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+    }
+    else
+    {
+        printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
     }
 
     return (0);
