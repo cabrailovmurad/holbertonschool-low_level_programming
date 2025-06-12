5618c1c192ed2cc7debfa236abda3fb635d73482
cabrailovmurad <dmurad908@gmail.com>
2025-06-12 16:50:57 +0000

    regeg

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,4 +1,3 @@
- GNU nano 6.2                                                                                                  1-last_digit.c                                                                                                           
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -8,25 +7,29 @@
  *
  * Return: Vsegda 0.
  */
- int main(void)
+int main(void)
 {
-int n;
-srand(time(0)); /* Initsializiruem generator sluchaynykh chisel */
-n = rand() - RAND_MAX / 2;/* Generiruyem sluchaynoye chislo ot -RAND_MAX/2 do +RAND_MAX/2 */
-if ((n % 10) > 5) /* Poluchayem poslednyuyu cifru chisla */
-{
-printf("Last digit of %d is %d and is greater than 5\n",
-n, n % 10);
-}
-else if ((n % 10) < 6 && (n % 10) != 0)
-{
-printf("Last digit of %d is %d and is less than 6 and not 0\n",
-n, n % 10);
-}
-else
-{
-printf("Last digit of %d is %d and is 0\n",
-n, n % 10);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
+
+	return (0);
+
 }
