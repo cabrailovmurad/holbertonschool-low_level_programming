769b93c1bd8a084ec203577f1d5d002e22753840
cabrailovmurad <dmurad908@gmail.com>
2025-06-13 15:41:45 +0000

    Add program that prints hexadecimal numbers in lowercase

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print all hexadecimal numbers in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+    char c;
+
+    for (c = '0'; c <= '9'; c++)
+        putchar(c);
+
+    for (c = 'a'; c <= 'f'; c++)
+        putchar(c);
+
+    putchar('\n');
+    return (0);
+}
