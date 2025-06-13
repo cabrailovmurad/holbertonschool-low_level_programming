ba97d8487998b1166a928ad8852e23499d2e540f
cabrailovmurad <dmurad908@gmail.com>
2025-06-13 17:45:03 +0000

    new line printing puchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main - prints "_putchar" using only putchar
- *
- * Return: Always 0
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char str[] = "_putchar";
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		putchar(str[i]);
-	}
-	putchar('\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
