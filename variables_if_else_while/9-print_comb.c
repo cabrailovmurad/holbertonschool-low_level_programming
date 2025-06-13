ac2d81443a1b1e14d394babee5be52516cd00710
cabrailovmurad <dmurad908@gmail.com>
2025-06-13 16:13:41 +0000

    dd program printing all single digit numbers separated by comma and spac

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar('0' + i);
-        if (i != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
