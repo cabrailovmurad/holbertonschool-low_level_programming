f333139c0efd77b25bb6a99609acc294f62b998a
cabrailovmurad <dmurad908@gmail.com>
2025-06-15 15:31:02 +0000

    Add program to sum even Fibonacci numbers <= 4,000,000

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even-valued Fibonacci terms <= 4000000
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    unsigned long int a = 1, b = 2, next, sum = 2;
+
+    while (1)
+    {
+        next = a + b;
+        if (next > 4000000)
+            break;
+
+        if (next % 2 == 0)
+            sum += next;
+
+        a = b;
+        b = next;
+    }
+
+    printf("%lu\n", sum);
+    return (0);
+}
