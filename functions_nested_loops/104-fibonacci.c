bf9f66016cc596fef201cc879a8b5220234255e9
cabrailovmurad <dmurad908@gmail.com>
2025-06-15 15:35:22 +0000

    Implement printing first 98 Fibonacci numbers without arrays or long long

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 98 Fibonacci numbers starting with 1 and 2
- *        separated by comma and space, followed by newline.
- * Return: Always 0.
+ * main - prints first 98 Fibonacci numbers, starting with 1 and 2
+ * Return: Always 0
  */
 int main(void)
 {
-	unsigned long a1 = 0, a2 = 1, b1 = 0, b2 = 2;
-	unsigned long c1, c2;
-	int i;
+    unsigned long int f1 = 1, f2 = 2, next;
+    int i;
 
-	printf("1, 2, ");
+    printf("%lu, %lu", f1, f2);
+    for (i = 3; i <= 98; i++)
+    {
+        next = f1 + f2;
+        printf(", %lu", next);
+        f1 = f2;
+        f2 = next;
+    }
+    printf("\n");
 
-	for (i = 3; i <= 98; i++)
-	{
-		c1 = a1 + b1;
-		c2 = a2 + b2;
-
-		if (c2 > 9999999999)
-		{
-			c1 += 1;
-			c2 %= 10000000000;
-		}
-
-		printf("%lu%010lu", c1, c2);
-
-		if (i != 98)
-			printf(", ");
-		else
-			printf("\n");
-
-		a1 = b1;
-		a2 = b2;
-		b1 = c1;
-		b2 = c2;
-	}
-
-	return (0);
+    return (0);
 }
