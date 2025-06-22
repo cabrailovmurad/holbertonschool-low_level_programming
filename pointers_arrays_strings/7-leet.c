7069e5d42d4eb3668aa472414fcf66b942cd918a
cabrailovmurad <dmurad908@gmail.com>
2025-06-22 17:56:48 +0000

    taskk7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,20 +8,20 @@
  */
 char *leet(char *s)
 {
-    char letters[] = "aAeEoOtTlL";
-    char numbers[] = "4433007711";
-    int i, j;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
-            if (s[i] == letters[j])
-            {
-                s[i] = numbers[j];
-                break;
-            }
-        }
-    }
-    return (s);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = numbers[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
