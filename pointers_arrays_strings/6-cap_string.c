ccbf436a689e748bc17c39afbc045c96a6e7f032
cabrailovmurad <dmurad908@gmail.com>
2025-06-22 17:51:08 +0000

    wfw

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		// Проверка: начало строки или после разделителя
+		/* Check if it's the first character and is lowercase */
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] -= ('a' - 'A');
