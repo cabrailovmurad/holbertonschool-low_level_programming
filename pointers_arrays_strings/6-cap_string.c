fc889c964912797b9c79a774eef0b594cf4efc1e
cabrailovmurad <dmurad908@gmail.com>
2025-06-22 17:40:45 +0000

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: The string to modify
+ *
+ * Return: The modified string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int j, is_sep;
+
+	while (str[i])
+	{
+		// Проверка: начало строки или после разделителя
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= ('a' - 'A');
+		}
+		else
+		{
+			is_sep = 0;
+			for (j = 0; sep[j]; j++)
+			{
+				if (str[i - 1] == sep[j])
+				{
+					is_sep = 1;
+					break;
+				}
+			}
+			if (is_sep && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= ('a' - 'A');
+			}
+		}
+		i++;
+	}
+	return (str);
+}
