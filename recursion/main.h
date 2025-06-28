9de5df77af13e77ea541d99ea6a2e048322bebf2
cabrailovmurad <dmurad908@gmail.com>
2025-06-28 15:33:18 +0000

    Add recursion tasks 0 to 3

--- recursion/main.h
+++ recursion/main.h
@@ -2,7 +2,9 @@
 #define MAIN_H
 
 void _puts_recursion(char *s);
-int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
+int _putchar(char c);
+
 #endif /* MAIN_H */
