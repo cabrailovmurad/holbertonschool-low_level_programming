3e74b74fc453a5ec924488a48a6c652d26a13bf0
cabrailovmurad <dmurad908@gmail.com>
2025-06-28 16:34:09 +0000

    task 1 f

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,18 @@
 
 /**
  * _memcpy - copies n bytes from memory area src to memory area dest
- * @dest: destination memory area
- * @src: source memory area
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
  * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        dest[i] = src[i];
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 
-    return (dest);
+	return (dest);
 }
