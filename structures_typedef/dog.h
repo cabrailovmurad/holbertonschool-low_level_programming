d944c3528cc2dbb3bf3a6bdca14ba66b2ddef883
cabrailovmurad <dmurad908@gmail.com>
2025-07-06 18:24:47 +0000

    dog task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,10 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef DOG
+#define DOG
 /**
- * struct dog - structure representing a dog
- * @name: name of the dog (string)
- * @age: age of the dog (float)
- * @owner: owner of the dog (string)
+ * struct dog - Create a struct of a dog
+ * @name: Dog's name
+ * @age: Age of the dog
+ * @owner: Dog's owner
  */
 struct dog
 {
@@ -14,6 +13,10 @@ struct dog
 	char *owner;
 };
 
-void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
