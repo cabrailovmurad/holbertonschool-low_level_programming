d72b71379b469983c081e0c1e681527b327648ef
cabrailovmurad <dmurad908@gmail.com>
2025-07-06 17:47:57 +0000

    1 task

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,40 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - concatenates two strings, using at most n bytes of s2
+ * string_nconcat - concatenates two strings, using at most n bytes from s2
  * @s1: first string
  * @s2: second string
- * @n: max number of bytes to use from s2
+ * @n: maximum bytes to use from s2
  *
- * Return: pointer to new allocated string or NULL on failure
+ * Return: pointer to newly allocated string, or NULL if failed
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len_s1 = 0, len_s2 = 0, len_concat = 0;
-    char *result;
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *result;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1)
+		while (s1[len1])
+			len1++;
+	if (s2)
+		while (s2[len2])
+			len2++;
 
-    len_s1 = strlen(s1);
-    len_s2 = strlen(s2);
+	if (n >= len2)
+		n = len2;
 
-    if (n >= len_s2)
-        n = len_s2;
+	result = malloc(len1 + n + 1);
+	if (!result)
+		return (NULL);
 
-    len_concat = len_s1 + n;
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
 
-    result = malloc(len_concat + 1);
-    if (result == NULL)
-        return (NULL);
+	for (j = 0; j < n; j++)
+		result[i + j] = s2[j];
 
-    memcpy(result, s1, len_s1);
-    memcpy(result + len_s1, s2, n);
-    result[len_concat] = '\0';
+	result[i + j] = '\0';
 
-    return (result);
+	return (result);
 }
+
