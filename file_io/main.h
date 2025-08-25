eb8ebc14d2491e95dee7faa8d353fb522b8444fb
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 14:31:16 +0000

    task 3

--- file_io/main.h
+++ file_io/main.h
@@ -4,9 +4,16 @@
 #include <stddef.h>
 #include <unistd.h>
 
-/* Function prototypes */
+/* POSIX standards */
+#include <unistd.h>
+
+/* OPEN, READ, WRITE, CLOSE */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-int _putchar(char c);
 #endif /* MAIN_H */
