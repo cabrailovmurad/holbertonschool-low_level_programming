1c9bd9c65bc491e1422781b29474c64bc12aca9d
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 13:39:42 +0000

    task5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    int first = 1;
+    hash_node_t *node;
+
+    if (!ht)
+        return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        node = ht->array[i];
+        while (node)
+        {
+            if (!first)
+                printf(", ");
+            printf("'%s': '%s'", node->key, node->value);
+            first = 0;
+            node = node->next;
+        }
+    }
+    printf("}\n");
+}
