99fd61b7b91a7d99f87c32a2e6b123c94c8ccea6
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 14:20:20 +0000

    task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 on success, exits with code on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUF_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		if (close(fd_from) == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+		exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUF_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			if (close(fd_from) == -1)
+				dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+			if (close(f
