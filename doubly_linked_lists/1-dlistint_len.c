3439ed8fd046a15ae870dffa9e17b61bb19520ad
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 12:27:09 +0000

    task 1

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a linked dlistint_t list
- * @h: pointer to the head of the list
- * Return: number of elements in the list
+ * dlistint_len - returns the number of elements in a dlistint_t list
+ * @h: pointer to head of list
+ *
+ * Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *current = h;
+	size_t count = 0;
 
-    /* Traverse the list and count nodes */
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
-
-    return (count);
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
