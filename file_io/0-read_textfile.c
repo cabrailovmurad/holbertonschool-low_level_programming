4589083e0153de5f77c525329c5da2ddccb17994
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 14:13:01 +0000

    task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,47 +1,51 @@
 #include "main.h"
 #include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to POSIX stdout
- * @filename: name of the file
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: file name
  * @letters: number of letters to read and print
- * Return: actual number of letters read and printed, 0 on error
+ * Return: actual number of letters read and printed, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t n_read, n_written;
-    char *buffer;
+	int fd;
+	ssize_t rd, wr;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (!buffer)
-    {
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    n_read = read(fd, buffer, letters);
-    if (n_read == -1)
-    {
-        free(buffer);
-        close(fd);
-        return (0);
-    }
+	rd = read(fd, buffer, letters);
+	if (rd == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    n_written = write(STDOUT_FILENO, buffer, n_read);
-    free(buffer);
-    close(fd);
+	wr = write(STDOUT_FILENO, buffer, rd);
+	if (wr != rd)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-    if (n_written != n_read)
-        return (0);
-
-    return (n_written);
+	free(buffer);
+	close(fd);
+	return (rd);
 }
