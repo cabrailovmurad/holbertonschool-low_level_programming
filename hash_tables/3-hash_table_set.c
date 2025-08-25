9c2efa34a6046528e7982a6a761aa7710361c939
cabrailovmurad <dmurad908@gmail.com>
2025-08-25 13:28:30 +0000

    task 3

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,75 @@
+#include "hash_tables.h"
+
+/**
+ * create_node - Creates a new hash node
+ * @key: key string
+ * @value: value string
+ *
+ * Return: Pointer to new node, or NULL on failure
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (NULL);
+
+	node->key = strdup(key);
+	if (!node->key)
+	{
+		free(node);
+		return (NULL);
+	}
+
+	node->value = strdup(value);
+	if (!node->value)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+
+	node->next = NULL;
+	return (node);
+}
+
+/**
+ * hash_table_set - Adds or updates an element in the hash table
+ * @ht: hash table
+ * @key: key string
+ * @value: value string
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *tmp, *new_node;
+
+	if (!ht || !key || *key == '\0' || !value)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (tmp->value ? 1 : 0);
+		}
+		tmp = tmp->next;
+	}
+
+	new_node = create_node(key, value);
+	if (!new_node)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
